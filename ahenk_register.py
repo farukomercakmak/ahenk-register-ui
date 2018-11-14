93f8b5c62d526beb41eee76c6d0152593160cebe
Ömer ÇAKMAK <farukomercakmak@gmail.com>
2018-11-14 14:36:35 +0300

    Update

--- ahenk_register.py
+++ ahenk_register.py
@@ -9,24 +9,33 @@ import configparser
 import easygui
 
 def register():
-    register_image = "/usr/share/ahenk/icons/ahenk_register.png"
-    msg = "Ahenk etki alanında değil, etki alanına almak istiyor musunuz?"
-    choices = ["Evet", "Daha Sonra"]
-    reply = easygui.buttonbox(msg, image=register_image, choices=choices)
-    if reply == "Evet":
-        command = "/usr/bin/python3 /usr/share/ahenk/ahenkd.py start"
-        process = subprocess.Popen(command, stdin=None, env=None, cwd=None, stderr=subprocess.PIPE, stdout=subprocess.PIPE, shell=True)
-        result_code = process.wait()
-        p_out = process.stdout.read().decode("unicode_escape")
-        p_err = process.stderr.read().decode("unicode_escape")
-    else:
-        print("Etki alanına alma işlemi iptal edildi")
+    try:
+        register_image = "/usr/share/ahenk/icons/ahenk_register.png"
+        msg = "Ahenk etki alanında değil, etki alanına almak istiyor musunuz?"
+        choices = ["Evet", "Daha Sonra"]
+        reply = easygui.buttonbox(msg, image=register_image, title='Ahenk Etki Alanı Kontrolü', choices=choices)
+        if reply == "Evet":
+
+
+            command = "/usr/bin/python3 /usr/share/ahenk/ahenkd.py start"
+
+            #command = 'su - {0} -c "{1}"'.format('foc-065035021', command)
+            #exec("/home/foc-065035021/dev/ahenk-register-ui/a.py")
+            process = subprocess.Popen(command, shell=True)
+            result_code = process.wait()
+            print(result_code)
+
+        else:
+            print("Etki alanına alma işlemi iptal edildi")
+
+    except Exception as e:
+        print(str(e))
 
 def unregister():
     register_image = "/usr/share/ahenk/icons/ahenk_unregister.png"
     msg = "Ahenk etki alanında, çıkmak istiyor musunuz?"
     choices = ["Evet", "Daha Sonra"]
-    reply = easygui.buttonbox(msg, image=register_image, choices=choices)
+    reply = easygui.buttonbox(msg, image=register_image, title='Ahenk Etki Alanı Kontrolü', choices=choices)
     if reply == "Evet":
         command = "/usr/bin/python3 /usr/share/ahenk/ahenkd.py unregister"
         process = subprocess.Popen(command, stdin=None, env=None, cwd=None, stderr=subprocess.PIPE, stdout=subprocess.PIPE, shell=True)
@@ -45,7 +54,7 @@ def ahenk_control():
     else:
 
         cevap = 'Lütfen ahenk paketinin yüklü olduğundan emin olunuz!'
-        choice = easygui.buttonbox(msg=cevap, image=rahenk_control_image, title='Ahenk Etki alanı Kontrolü',choices=['Tamam'])
+        choice = easygui.buttonbox(msg=cevap, image=rahenk_control_image, title='Ahenk Etki Alanı Kontrolü',choices=['Tamam'])
 
 def uid():
     config = configparser.ConfigParser()
