9b781fd6ecdb7b68e3f5cb33c1c574b46cdd997e
Ömer ÇAKMAK <farukomercakmak@gmail.com>
2018-11-21 08:45:29 +0300

    Update

--- Makefile
+++ Makefile
@@ -12,10 +12,15 @@ install:
 
 	mkdir -p $(DESTDIR)/usr/bin
 	@cp -rf ahenk-register $(DESTDIR)/usr/bin/
+
+	mkdir -p $(DESTDIR)/usr/sbin
+        @cp -rf ahenk-register-pkexec $(DESTDIR)/usr/sbin/
+
 uninstall:
 	@rm -rf /usr/share/ahenk/ahenk_register.py
 	@rm -rf /usr/share/applications/ahenk.desktop
 	@rm -rf /usr/bin/ahenk/icons
 	@rm -rf /usr/bin/ahenk-register
+	@rm -rf /usr/sbin/ahenk-register-pkexec
 
 .PHONY: install uninstall
--- ahenk_register.py
+++ ahenk_register.py
@@ -10,10 +10,10 @@ import easygui
 
 def register():
     try:
-        register_image = "/usr/share/ahenk/icons/ahenk_register.png"
+
         msg = "Ahenk etki alanında değil, etki alanına almak istiyor musunuz?"
         choices = ["Evet", "Daha Sonra"]
-        reply = easygui.buttonbox(msg, image=register_image, title='Ahenk Etki Alanı Kontrolü', choices=choices)
+        reply = easygui.buttonbox(msg, title='Ahenk Etki Alanı Kontrolü', choices=choices)
         if reply == "Evet":
             command = "/usr/bin/python3 /usr/share/ahenk/ahenkd.py start"
             process = subprocess.Popen(command, shell=True)
@@ -26,10 +26,10 @@ def register():
         print(str(e))
 
 def unregister():
-    register_image = "/usr/share/ahenk/icons/ahenk_unregister.png"
+
     msg = "Ahenk etki alanında, çıkmak istiyor musunuz?"
     choices = ["Evet", "Daha Sonra"]
-    reply = easygui.buttonbox(msg, image=register_image, title='Ahenk Etki Alanı Kontrolü', choices=choices)
+    reply = easygui.buttonbox(msg, title='Ahenk Etki Alanı Kontrolü', choices=choices)
     if reply == "Evet":
         command = "/usr/bin/python3 /usr/share/ahenk/ahenkd.py unregister"
         process = subprocess.Popen(command, stdin=None, env=None, cwd=None, stderr=subprocess.PIPE, stdout=subprocess.PIPE, shell=True)
@@ -41,14 +41,14 @@ def unregister():
 
 def ahenk_control():
     PATH = '/etc/ahenk/ahenk.conf'
-    rahenk_control_image = "/usr/share/ahenk/icons/ahenk_register.png"
+
     if os.path.isfile(PATH) and os.access(PATH, os.R_OK):
 
         is_registered()
     else:
 
         cevap = 'Lütfen ahenk paketinin yüklü olduğundan emin olunuz!'
-        choice = easygui.buttonbox(msg=cevap, image=rahenk_control_image, title='Ahenk Etki Alanı Kontrolü',choices=['Tamam'])
+        choice = easygui.buttonbox(msg=cevap,  title='Ahenk Etki Alanı Kontrolü',choices=['Tamam'])
 
 def uid():
     config = configparser.ConfigParser()
